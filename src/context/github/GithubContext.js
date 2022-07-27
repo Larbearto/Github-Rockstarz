8409e90646d08abb6a862905577c5d1bff571e60
SpiderBear <SpiderBear714@gmail.com>
2022-07-27 01:34:03 -0700

    refactored actions context, add axios, clean up code

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,48 +3,21 @@ import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
+		user: {},
+		repos: [],
 		loading: false,
 	}
 
 	const [state, dispatch] = useReducer(githubReducer, initialState)
 
-	// Set loading
-	const setLoading = () => dispatch({ type: 'SET_LOADING' })
-
-	// Get search Results
-	const searchUsers = async (text) => {
-		setLoading()
-
-		const params = new URLSearchParams({
-			q: text,
-		})
-
-		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-			headers: {
-				Authorization: `token ${GITHUB_TOKEN}`,
-			},
-		})
-
-		const { items } = await response.json()
-
-		dispatch({
-			type: 'GET_USERS',
-			payload: items,
-		})
-	}
-
 	return (
 		<GithubContext.Provider
 			value={{
-				users: state.users,
-				loading: state.loading,
-				searchUsers,
+				...state,
+				dispatch,
 			}}
 		>
 			{children}
